a60a3ff5cf1beee762c2fec699c9e199f99c6a54
raawitez <raawiteza123@gmail.com>
2026-06-09 18:01:11 +0530

    make database configuration environment-based for Docker support

--- notification-api/app/database.py
+++ notification-api/app/database.py
@@ -1,11 +1,20 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./notification.db"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///./notification.db")
+
+connect_args=(
+    {"check_same_thread": False}
+    if DATABASE_URL.startswith("sqlite")
+    else{}
+)
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    connect_args=connect_args
 )
 
 SessionLocal = sessionmaker(
